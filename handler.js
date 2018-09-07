0bd51764574f9b9947eceff0c2e8f7656dd708bc
ohmaye <enio.ohmaye@gmail.com>
2018-09-07 15:05:55 +0200

    Finally! API takes the parameters properly through an API Gateway, passes to Python lambda, and returns result

--- handler.js
+++ handler.js
@@ -1,41 +1,54 @@
 "use strict";
-const AWS = require("aws-sdk")
+const AWS = require("aws-sdk");
 
-const lambda = new AWS.lambda({region:"us-east-1"})
-const message_string = {}
-const params = {
-  FunctionName: "writingCheck-dev-writingCheck",
-  InvocationType: "RequestResponse",
-  Payload: JSON.stringify({"writing":"hello","sample":"hi"})
-}
-lambda.invoke(params, (error, data) => {
-  console.log("Done calling lambda")
-})
+const lambda = new AWS.Lambda({ region: "us-east-1" });
+
+//const message_string = {};
+
+
 
 module.exports.endpoint = (event, context, callback) => {
-  let writing = '***'
-  let sample = '!!!'
+  let writing = "***";
+  let sample = "!!!";
 
   if (event.body !== null && event.body !== undefined) {
-    let body = JSON.parse(event.body)
+    let body = JSON.parse(event.body);
     writing = body.writing;
     sample = body.sample;
   }
 
   let termFrequencyResult = 0;
+  let result = "No result returned.";
+  let response = {}
 
-
-
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: {
-        TermFrequency: termFrequencyResult,
-        Student: writing,
-        Sample: sample
-      }
-    })
+  const params = {
+    FunctionName: "termfrequencylambda-dev-termFrequency",
+    InvocationType: "RequestResponse",
+    Payload: JSON.stringify({ writing: writing, sample: sample})
   };
 
-  callback(null, response);
+  lambda.invoke(params, (error, data) => {
+    if (error) {
+      console.log("Error calling lambda");
+    } else {
+      console.log(data.Payload)
+      result = data.Payload
+      response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: {
+            TermFrequency: termFrequencyResult,
+            Student: writing,
+            Sample: sample,
+            Data: result
+          }
+        })
+      }
+      if (data.Payload) {
+        result = data.Payload;
+      }
+      callback(null, response);
+    }
+  });
+
 };
