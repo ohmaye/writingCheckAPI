e33c5591c46cb827646310d7956a031196fe3740
ohmaye <enio.ohmaye@gmail.com>
2018-09-12 14:15:09 +0200

    works with writingCheck page and termFrequency

--- handler.js
+++ handler.js
@@ -1,48 +1,49 @@
-"use strict";
-const AWS = require("aws-sdk");
+"use strict"
+const AWS = require("aws-sdk")
 
-const lambda = new AWS.Lambda({ region: "us-east-1" });
-
+const lambda = new AWS.Lambda({ region: "us-east-1" })
 
 module.exports.endpoint = (event, context, callback) => {
-  let writing = "*";
-  let sample = "!";
+  let writing = "*"
+  let sample = "!"
 
   if (event.body !== null && event.body !== undefined) {
-    let body = JSON.parse(event.body);
-    writing = body.writing;
-    sample = body.sample;
+    let body = JSON.parse(event.body)
+    writing = body.writing
+    sample = body.sample
   }
 
-  let termFrequencyResult = 0;
-  let result = "No result returned.";
+  let termFrequencyResult = 0
+  let result = "No result returned."
   let response = {}
 
   const params = {
     FunctionName: "termfrequencylambda-dev-termFrequency",
     InvocationType: "RequestResponse",
-    Payload: JSON.stringify({ writing: writing, sample: sample})
-  };
+    Payload: JSON.stringify({ writing: writing, sample: sample })
+  }
 
   lambda.invoke(params, (error, data) => {
     if (error) {
-      console.log("Error calling lambda");
+      console.log("Error calling lambda")
     } else {
       console.log(data.Payload)
-      result = data.Payload
+      result = JSON.parse(data.Payload)
       response = {
         statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true
+        },
         body: JSON.stringify({
           message: {
-            TermFrequency: termFrequencyResult,
-            Student: writing,
-            Sample: sample,
-            Data: result
+            termFrequency: result.termFrequency,
+            student: result.student_writing,
+            sample: result.school_sample
           }
         })
       }
-      callback(null, response);
+      callback(null, response)
     }
-  });
-
-};
+  })
+}
