591157dc68f1f4317ede575dcbd93e06b05d253a
ohmaye <enio.ohmaye@gmail.com>
2018-09-25 15:33:53 +0200

    Finally working with DynamoDB

--- handlerdb.js
+++ handlerdb.js
@@ -1,77 +1,79 @@
 "use strict"
 const AWS = require("aws-sdk")
-AWS.config.update({region: 'us-east-1'})
+const { Observable, from, just } = require("rxjs")
+const { map } = require("rxjs/operators")
+
+// Read SAMPLE from the TemplateCourseItem dynamodb table
+const TABLE_NAME = "TemplateCourseItem"
+
+AWS.config.update({ region: "us-east-1" })
 
 const lambda = new AWS.Lambda({ region: "us-east-1" })
 const dynamoDB = new AWS.DynamoDB.DocumentClient()
 
+let writing = "*"
+let TemplateCourseItem_id = null
+let CourseVersion = null
+let sample = "!"
+
+let termFrequencyResult = 0
+let result = "No result returned."
+let response = {}
+
 module.exports.endpoint = (event, context, callback) => {
-  let writing = "*"
-  let TemplateCourseItem_id = null
-  let CourseVersion = null
-  let sample = "!"
-/*
+  
   if (event.body !== null && event.body !== undefined) {
     let body = JSON.parse(event.body)
     writing = body.writing
     TemplateCourseItem_id = body.TemplateCourseItem_id
     CourseVersion = body.CourseVersion
   }
-  */
-
-  // Read SAMPLE from the TemplateCourseItem table
-  const TABLE_NAME = 'TemplateCourseItem'
+  
 
   const paramsDB = {
-    'TableName': TABLE_NAME,
-    'Key': {
-        "TemplateCourseItem_id":  "101008530",
-        "CourseVersion":  "2.0.0.1"
+    TableName: TABLE_NAME,
+    Key: {
+      TemplateCourseItem_id: TemplateCourseItem_id,
+      CourseVersion: CourseVersion
     },
-    "ProjectionExpression": "ModelTxtUrl"
+    ProjectionExpression: "ModelTxtUrl"
   }
 
-  dynamoDB.get( paramsDB, (error, result) => {
+  dynamoDB.get(paramsDB, (error, result) => {
     // Handle errors
     if (error) {
       console.log("Error reading DB")
       return
     }
-    sample = JSON.stringify(result)
-  })
-
-
-  let termFrequencyResult = 0
-  let result = "No result returned."
-  let response = {}
-
-  const params = {
-    FunctionName: "termfrequencylambda-dev-termFrequency",
-    InvocationType: "RequestResponse",
-    Payload: JSON.stringify({ writing: writing, sample: sample })
-  }
-
-  lambda.invoke(params, (error, data) => {
-    if (error) {
-      console.log("Error calling lambda")
-    } else {
-      console.log(data.Payload)
-      result = JSON.parse(data.Payload)
-      response = {
-        statusCode: 200,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Credentials": true
-        },
-        body: JSON.stringify({
-          message: {
-            termFrequency: result.termFrequency,
-            student: result.student_writing,
-            sample: result.school_sample
-          }
-        })
-      }
-      callback(null, response)
+    sample = JSON.stringify(result.Item.ModelTxtUrl)
+    const params = {
+      FunctionName: "termfrequencylambda-dev-termFrequency",
+      InvocationType: "RequestResponse",
+      Payload: JSON.stringify({ writing: writing, sample: sample })
     }
+
+    lambda.invoke(params, (error, data) => {
+      if (error) {
+        console.log("Error calling lambda")
+      } else {
+        console.log(data.Payload)
+        result = JSON.parse(data.Payload)
+        response = {
+          statusCode: 200,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true
+          },
+          body: JSON.stringify({
+            message: {
+              termFrequency: result.termFrequency,
+              student: result.student_writing,
+              sample: result.school_sample
+            }
+          })
+        }
+        callback(null, response)
+      }
+    })
   })
 }
